3df87f33aaf3ff68229583d368face813deeb6e2
Kevin Berry <kevin.berry@improving.com>
2025-01-02 09:47:30 -0600

    Make summary match temperature.

--- Program.cs
+++ Program.cs
@@ -16,21 +16,33 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
 app.MapGet("/weatherforecast", () =>
 {
     var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
+        {
+            var temperature = Random.Shared.Next(-20, 55);
+            var description = temperature switch
+            {
+                var x when x <= 0 => "Freezing",
+                var x when x >  0 && x <= 10 => "Bracing",
+                var x when x > 10 && x <= 15 => "Chilly",
+                var x when x > 15 && x <= 20 => "Cool",
+                var x when x > 20 && x <= 23 => "Mild",
+                var x when x > 23 && x <= 27 => "Warm",
+                var x when x > 27 && x <= 30 => "Balmy",
+                var x when x > 30 && x <= 40 => "Hot",
+                var x when x > 40 && x <= 45 => "Sweltering",
+                _ => "Scorching"
+            };
+
+            return new WeatherForecast(
+                DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                temperature,
+                description
+            );
+        })
         .ToArray();
+
     return forecast;
 })
 .WithName("GetWeatherForecast")
